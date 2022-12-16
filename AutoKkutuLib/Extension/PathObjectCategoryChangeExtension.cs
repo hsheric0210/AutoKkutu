366a595dc477418aed85981efb27260edc8e9a1a
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2022-12-16 19:35:53 +0900

    ...Forgot some files

---
+++ AutoKkutuLib/Extension/PathObjectCategoryChangeExtension.cs
@@ -0,0 +1,83 @@
+﻿using AutoKkutuLib.Database;
+using AutoKkutuLib.Database.Extension;
+using Serilog;
+
+namespace AutoKkutuLib.Extension;
+public static class PathObjectCategoryChangeExtension
+{
+	public static void MakeAttack(this AbstractDatabaseConnection connection, PathObject pathObject, GameMode mode)
+	{
+		if (pathObject.Categories.HasFlag(WordCategories.AttackWord))
+			return;
+
+		var node = pathObject.ToNode(mode);
+		connection.DeleteNode(node, GetEndWordListTableName(mode));
+		if (connection.AddNode(node, GetAttackWordListTableName(mode)))
+			Log.Information(I18n.PathMark_Success, node, I18n.PathMark_Attack, mode);
+		else
+			Log.Warning(I18n.PathMark_AlreadyDone, node, I18n.PathMark_Attack, mode);
+	}
+
+	public static void MakeEnd(this AbstractDatabaseConnection connection, PathObject pathObject, GameMode mode)
+	{
+		if (pathObject.Categories.HasFlag(WordCategories.EndWord))
+			return;
+
+		var node = pathObject.ToNode(mode);
+		connection.DeleteNode(node, GetAttackWordListTableName(mode));
+		if (connection.AddNode(node, GetEndWordListTableName(mode)))
+			Log.Information(I18n.PathMark_Success, node, I18n.PathMark_End, mode);
+		else
+			Log.Warning(I18n.PathMark_AlreadyDone, node, I18n.PathMark_End, mode);
+	}
+
+	public static void MakeNormal(this AbstractDatabaseConnection connection, PathObject pathObject, GameMode mode)
+	{
+		if (!pathObject.Categories.HasFlag(WordCategories.EndWord) && !pathObject.Categories.HasFlag(WordCategories.AttackWord))
+			return;
+
+		var node = pathObject.ToNode(mode);
+		var endWord = connection.DeleteNode(node, GetEndWordListTableName(mode)) > 0;
+		var attackWord = connection.DeleteNode(node, GetAttackWordListTableName(mode)) > 0;
+		if (endWord || attackWord)
+			Log.Information(I18n.PathMark_Success, node, I18n.PathMark_Normal, mode);
+		else
+			Log.Warning(I18n.PathMark_AlreadyDone, node, I18n.PathMark_Normal, mode);
+	}
+
+	private static string GetAttackWordListTableName(GameMode mode) => mode switch
+	{
+		GameMode.FirstAndLast => DatabaseConstants.ReverseAttackNodeIndexTableName,
+		GameMode.Kkutu => DatabaseConstants.KkutuAttackNodeIndexTableName,
+		_ => DatabaseConstants.AttackNodeIndexTableName,
+	};
+
+	private static string GetEndWordListTableName(GameMode mode) => mode switch
+	{
+		GameMode.FirstAndLast => DatabaseConstants.ReverseEndNodeIndexTableName,
+		GameMode.Kkutu => DatabaseConstants.KkutuEndNodeIndexTableName,
+		_ => DatabaseConstants.EndNodeIndexTableName,
+	};
+
+	private static string ToNode(this PathObject pathObject, GameMode mode)
+	{
+		string content = pathObject.Content;
+		switch (mode)
+		{
+			case GameMode.FirstAndLast:
+				return content.GetFaLTailNode();
+
+			case GameMode.MiddleAndFirst:
+				if (content.Length % 2 == 1)
+					return content.GetMaFTailNode();
+				break;
+
+			case GameMode.Kkutu:
+				if (content.Length > 2)
+					return content.GetKkutuTailNode();
+				break;
+		}
+
+		return content.GetLaFTailNode();
+	}
+}
---
+++ AutoKkutuLib/Extension/WordToNodeExtension.cs
@@ -0,0 +1,50 @@
+﻿namespace AutoKkutuLib.Extension;
+
+public static class WordToNodeExtension
+{
+	/// <summary>
+	/// 끝말잇기 단어 <paramref name="word"/>의 HEAD 노드/인덱스
+	/// </summary>
+	public static string GetLaFHeadNode(this string word) => word == null ? throw new ArgumentNullException(nameof(word)) : word[0].ToString();
+
+	/// <summary>
+	/// 앞말잇기 단어 <paramref name="word"/>의 HEAD 노드/인덱스
+	/// </summary>
+	public static string GetFaLHeadNode(this string word) => word == null ? throw new ArgumentNullException(nameof(word)) : word.Last().ToString();
+
+	/// <summary>
+	/// 끄투 단어 <paramref name="word"/>의 HEAD 노드/인덱스
+	/// </summary>
+	public static string GetKkutuHeadNode(this string word)
+	{
+		if (word == null)
+			throw new ArgumentNullException(nameof(word));
+
+		return word.Length >= 4 ? word[..2] : word.Length >= 3 ? word[0].ToString() : "";
+	}
+
+	/// <summary>
+	/// 끝말잇기 단어 <paramref name="word"/>의 TAIL 노드/인덱스
+	/// </summary>
+	public static string GetLaFTailNode(this string word) => word.GetFaLHeadNode();
+
+	/// <summary>
+	/// 앞말잇기 단어 <paramref name="word"/>의 TAIL 노드/인덱스
+	/// </summary>
+	public static string GetFaLTailNode(this string word) => word.GetLaFHeadNode();
+
+	/// <summary>
+	/// 끄투 단어 <paramref name="word"/>의 TAIL 노드/인덱스
+	/// </summary>
+	public static string GetKkutuTailNode(this string word)
+	{
+		return word == null
+			? throw new ArgumentNullException(nameof(word))
+			: word.Length >= 4 ? word.Substring(word.Length - 3, 2) : word.Last().ToString();
+	}
+
+	/// <summary>
+	/// 가운뎃말잇기 단어 <paramref name="word"/>의 TAIL 노드/인덱스
+	/// </summary>
+	public static string GetMaFTailNode(this string word) => word == null ? throw new ArgumentNullException(nameof(word)) : word[(word.Length - 1) / 2].ToString();
+}
